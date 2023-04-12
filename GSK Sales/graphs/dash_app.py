3814637f5cdabed439bac1c617c7c8207d17db8f
nazim <nazim.benammar@gmail.com>
2023-04-12 12:20:34 +0100

    Dash V0.1

--- GSK Sales/graphs/dash_app.py
+++ GSK Sales/graphs/dash_app.py
@@ -1,4 +1,27 @@
+from datetime import datetime
+
+import pandas as pnd
 from dash import html
+
+
+class PeriodsDates:
+
+    @staticmethod
+    def get_weeks_ends(end_date: pnd.Timestamp) -> dict[str, pnd.Timestamp]:
+        start_date = datetime(year=end_date.year, month=1, day=1, hour=16)
+        year_weeks_dates: pnd.DatetimeIndex = pnd.date_range(start_date, end_date, freq='W-THU')
+        weeks_dict: [str, pnd.Timestamp] = {}
+        month = 1
+        week_number = 1
+        for week in year_weeks_dates:
+            if week.month != month:
+                month = week.month
+                week_number = 1
+            week.strftime('%B')
+            key: str = f"{week.strftime('%B')} - W{week_number} (au {week.day}/{week.month})"
+            week_number = week_number + 1
+            weeks_dict.update({key: week})
+        return weeks_dict
 
 
 class Naming:
@@ -7,10 +30,7 @@ class Naming:
     orientation = 'ORIENTATION'
 
 
-class DashScene:
-    def __int__(self, name: str, scene: dict[str:dict]):
+class DashScreen:
+    def __int__(self, name: str, div: html.Div):
         self.name: str = name
-        self.scene: html.Div = scene
-
-    def load(self):
-        self.scene
+        self.Div: html.Div = div
